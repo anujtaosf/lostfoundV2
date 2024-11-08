0e5048213dcec2f14f7c09d3db7f42d50249c048
Your Name <ljweaver@umich.edu>
2024-11-07 22:15:09 -0500

    auto deploy test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,6 +15,7 @@ const Header = () => {
       <UserInfo>
         <UserIcon src={"https://cdn.builder.io/api/v1/image/assets/TEMP/1394bf55adad9bce9b5db46599eb47259925b3b28609188c86776a691a908cdd?placeholderIfAbsent=true&apiKey=74fbfc420745470bbcfc2ad34496c208"} alt="User avatar" />
       </UserInfo>
+      Testing Auto Deploy
       <WelcomeMessage>
         <Greeting>Good Morning{user ? `, ${user.displayName}` : ""}</Greeting>
         <DashboardInfo>
