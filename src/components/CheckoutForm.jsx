d530a0629237004f4d1c08bd5a9a643106aaf3d2
Anuhea Tao <anuhea@umich.edu>
2024-11-14 04:40:45 -0500

    checkin and checkout styled

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -3,6 +3,7 @@ import { getAllTools, getTool } from "../firebase/tools";
 import { getUniqname } from "../firebase/users";
 import { useAuth } from "../context/authContext";
 import { createTicket } from "../firebase/ticket";
+import styled from 'styled-components';
 
 
 const CheckoutForm = () => {
@@ -50,30 +51,110 @@ const CheckoutForm = () => {
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<label htmlFor="dropdown">Location:</label>
-			<select id="dropdown" value={selectedLocation} onChange={(e) => {setSelectedLocation(e.target.value)}}>
-				<option value="">-- Please choose an location --</option>
-				<option value="wilson">Wilson Center</option>
-				<option value="frb">FRB Makerspace</option>
-			</select>
-			<br/>
+		<Container>
+			<Checkout onSubmit={handleSubmit}>
+				<Formlabel>Tool Check-out Form</Formlabel>
+				<Description>Please choose the tool you are checking out and your location below</Description>
+				<Fieldlabel htmlFor="dropdown">Location:</Fieldlabel>
+				<Select id="dropdown" value={selectedLocation} onChange={(e) => {setSelectedLocation(e.target.value)}}>
+					<option value="">-- Please choose an location --</option>
+					<option value="wilson">Wilson Center</option>
+					<option value="frb">FRB Makerspace</option>
+				</Select>
+				<br/>
 
-			<label htmlFor="dropdown">Tool:</label>
-			<select id="dropdown" value={selectedTool} onChange={(e) => {setSelectedTool(e.target.value)}}>
-				<option value="">-- Please choose an option --</option>
-				{tools.map((item, index) => (
-					<option key={index} value={item}>
-						{item}
-					</option>
-				))}
-			</select>
-			<br/>
-			<button type="submit" disabled={!selectedTool || !selectedLocation}>
-				Submit
-			</button>
-		</form>
+				<Fieldlabel htmlFor="dropdown">Tool:</Fieldlabel>
+				<Select id="dropdown" value={selectedTool} onChange={(e) => {setSelectedTool(e.target.value)}}>
+					<option value="">-- Please choose an option --</option>
+					{tools.map((item, index) => (
+						<option key={index} value={item}>
+							{item}
+						</option>
+					))}
+				</Select>
+				<br/>
+				<SubmitButton type="submit" disabled={!selectedTool || !selectedLocation}>
+					Submit
+				</SubmitButton>
+			</Checkout>
+		</Container>
 	);
 };
+
+
+const Container = styled.div`
+	width: 100%;
+	padding: 40px 20px;
+	display: flex;
+	justify-content: center;
+`;
+
+const Checkout = styled.form`
+	width: 100%;
+	max-width: 480px;
+	padding: 32px;
+	background-color: white;
+	border-radius: 12px;
+	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.4);
+	display: flex;
+	flex-direction: column;
+	gap: 4px;
+`;
+
+const Formlabel = styled.label`
+	font-size: 32px;
+	font-weight: bold;
+	margin: 10px 0px 0px 0px;
+	
+`;
+
+const Description = styled.label`
+	font-size: 18px;
+	margin: 0px 10px 0px 0px;
+`;
+
+const Fieldlabel = styled.label`
+	font-size: 24px;
+	font-weight: bold;
+	margin: 10px 10px;
+	
+`;
+
+const Select = styled.select`
+	padding: 8px;
+	margin-bottom: 10px;
+	width: 100%;
+	max-width: 300px;
+	display: center;
+	border-radius: 6px;
+
+	&:focus {
+    outline: none;
+    box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.1);
+  	}
+	
+`;
+
+const SubmitButton = styled.button`
+	padding: 10px 20px;
+	font-size: 14px;
+	background-color: #4CAF50;
+	color: white;
+	border: none;
+	border-radius: 8px;
+	cursor: pointer;
+	margin: 10px;
+	transition: all 0.2s ease;
+
+
+	&:disabled {
+		background-color: #ccc;
+		cursor: not-allowed;
+	}
+	&:hover:not(:disabled) {
+    transform: scale(1.05);
+  	}
+
+`;
 
 export default CheckoutForm;
