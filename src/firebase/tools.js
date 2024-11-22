571973bd85d2dbb02668aa2ade0669fac932bfc5
Your Name <ljweaver@umich.edu>
2024-11-22 15:30:28 -0500

    firebase functions documentation

--- src/firebase/tools.js
+++ src/firebase/tools.js
@@ -2,6 +2,18 @@ import { FirebaseError } from "firebase/app";
 import { db } from "./firebase";
 import { collection, getDocs, query, where } from "firebase/firestore";
 
+/**
+ * @typedef tool
+ * @property {String} name name of tool
+ * @property {Number} amount amount in tool shop
+ * @property {Number} rating tool rating
+ * @property {String} training required training to checkout tool
+ *
+ */
+
+/**
+ * @returns {Tool[]} list of all tools in databasde
+ */
 export const getAllTools = async () => {
     try {
         const querySnapshot = await getDocs(collection(db, "tools"));
@@ -21,6 +33,10 @@ export const getAllTools = async () => {
     } 
 }
 
+/**
+ * @param {String} name 
+ * @returns {Tool} gets tool information by name
+ */
 export const getTool = async (name) => {
     try {
         const q = query(collection(db, "tools"), where("name", "==", name))
